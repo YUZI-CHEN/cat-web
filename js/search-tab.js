cab35440ac08e0781d64b56fbdc537b1f444dc73
yuxzi <kitty841002@gmail.com>
2025-01-10 16:17:23 +0800

    新增點

--- js/search-tab.js
+++ js/search-tab.js
@@ -1,39 +1,5 @@
 
-// function toggleLine(lineId) {
-//   // 隱藏所有捷運線
-//   const lines = document.querySelectorAll('.line-group');
-//   lines.forEach(line => line.classList.remove('active'));
 
-//   // 如果選中的捷運線已經顯示，則隱藏它；否則顯示
-//   const selectedLine = document.getElementById(lineId);
-//   if (selectedLine && !selectedLine.classList.contains('active')) {
-//     selectedLine.classList.add('active');
-//   }
-// }
-
-// function toggleDimBackground(isDimmed) {
-//   const bgElement = document.querySelector('.bg');
-//   if (isDimmed) {
-//     bgElement.classList.add('dimmed');
-//   } else {
-//     bgElement.classList.remove('dimmed');
-//   }
-// }
-
-// function toggleTag(tagElement) {
-//   const isSelected = tagElement.classList.toggle('selected');
-//   const lineId = tagElement.getAttribute('data-line-id');
-
-//   // 切換背景暗淡狀態
-//   toggleDimBackground(isSelected);
-
-//   // 如果選中標籤，顯示對應捷運線；否則隱藏所有
-//   if (isSelected) {
-//     toggleLine(lineId);
-//   } else {
-//     toggleLine(null); // 無參數，隱藏所有捷運線
-//   }
-// }
 function toggleLine(lineId, isSelected) {
   const selectedLine = document.getElementById(lineId);
 
